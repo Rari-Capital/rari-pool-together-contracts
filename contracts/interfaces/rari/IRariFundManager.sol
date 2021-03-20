f202da0cbd763c5d6b41cc6e607b874c595a4076
JetDeveloping <jetjadeja@gmail.com>
2021-03-20 11:04:11 -0700

    Added interfaces

---
+++ contracts/interfaces/rari/IRariFundManager.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.7.3;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IRariFundManager {
+    function balanceOf(address) external returns (uint256);
+    function rariFundToken() external view returns (IERC20);
+    
+    function deposit() payable external returns (bool);
+    function withdraw(uint256) external returns (bool);
+}
