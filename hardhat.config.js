417f1c6f10961afcf3884104892852fd63ce9422
JetDeveloping <jetjadeja@gmail.com>
2021-03-20 19:24:31 -0700

    Installed testing deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.FORKING_URL,
+        blockNumber: 12077686,
+      },
+      blockNumber: 12077686,
+    }
+  }
 };
